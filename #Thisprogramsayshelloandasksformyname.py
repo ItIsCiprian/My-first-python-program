59a3fd20a18ac97e8c3c5201d1592828f8b46ae4
ionutcipriananescu <ionutcipriananescu@gmail.com>
2015-09-29 14:09:34 +0300

    First commit

---
+++ #Thisprogramsayshelloandasksformyname.py
@@ -0,0 +1,15 @@
+#	This	program	says	hello	and	asks	for	my	name.
+print('Hello	world!')
+print('What	is	your	name?')				#	ask	for	their	name
+myName	=	input()
+print('It	is	good	to	meet	you,	'	+	myName)
+print('The	length	of	your	name	is:')
+print(len(myName))
+print('What	is	your	age?')				#	ask	for	their	age
+myAge	=	input()
+print('You	will	be	'	+	str(int(myAge)	+	1)	+	'	in  a  year.')
+print('You	will	be	'	+	str(int(myAge)	+	2)	+	'	in  two  years.')
+print('You	will	be	'	+	str(int(myAge)	+	3)	+	'	in  three  years.')
+print('You	will	be	'	+	str(int(myAge)	+	4)	+	'	in  four  years.')
+print('You	will	be	'	+	str(int(myAge)	+	5)	+	'	in  five  years.')# -This-program-says-hello-and-asks-for-my-name.
+# My-first-python-program
